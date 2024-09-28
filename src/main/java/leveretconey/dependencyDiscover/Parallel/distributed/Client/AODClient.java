731cb47f990972a8960fc32823adbae499caa6ba
Tunaccino <max.glaetzner2000@gmail.com>
2024-09-28 18:59:23 +0200

    .

--- src/main/java/leveretconey/dependencyDiscover/Parallel/distributed/Client/AODClient.java
+++ src/main/java/leveretconey/dependencyDiscover/Parallel/distributed/Client/AODClient.java
@@ -19,10 +19,10 @@ public class AODClient {
 
     public static void main(String[] args){
         AODClient client = new AODClient();
-        client.run("192.168.178.25","data/exp10","data/exp8 solutions");
+        client.run("192.168.178.25","data/exp10","data/exp8 solutions",true);
     }
 
-    public void run(String ip, String input, String output){
+    public void run(String ip, String input, String output,Boolean dontUseNull){
         try{
             AODService service = (AODService) Naming.lookup("rmi://"+ ip +":1099/AODService");
 
@@ -35,7 +35,116 @@ public class AODClient {
 
             Thread localProcessingThread = new Thread(() ->{
                 RunParallel runner = new RunParallel(filesUpper, output);
-                runner.runParallel();
+                runner.runParallel(dontUseNull);
+
+
+            });
+
+            Thread remoteProcessingThread = new Thread(() -> {
+                try{
+                    collections.set(service.processWebTable(Arrays.stream(filesLower)
+                            .map(File::getPath)
+                            .toArray(String[]::new), output));
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            });
+
+            localProcessingThread.start();
+            remoteProcessingThread.start();
+
+            localProcessingThread.join();
+            remoteProcessingThread.join();
+
+            for (Pair<Collection<LexicographicalOrderDependency>,String> pair : collections.get()){
+                ArrayList<String> lines = new ArrayList<>();
+                for (LexicographicalOrderDependency lod : pair.getKey()){
+                    lines.add(lod.toString());
+                }
+
+                try {
+                    String name = pair.getValue().substring(11);
+                    Files.write(Paths.get(output +"/"+ name),lines);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            System.out.println("Done calculating OD's!");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void runConvert(String ip, String input, String output,Boolean filter,Boolean dontUseNull){
+        try{
+            AODService service = (AODService) Naming.lookup("rmi://"+ ip +":1099/AODService");
+
+            File directory = new File(input);
+            File[] files = directory.listFiles();
+
+            File[] filesLower = Arrays.copyOfRange(files,0,files.length/2);
+            File[] filesUpper = Arrays.copyOfRange(files,files.length/2,files.length);
+            AtomicReference<ArrayList<Pair<Collection<LexicographicalOrderDependency>,String>>> collections = new AtomicReference<>(new ArrayList<>());
+
+            Thread localProcessingThread = new Thread(() ->{
+                RunParallel runner = new RunParallel(filesUpper, output);
+                runner.runParallel(dontUseNull);
+
+
+            });
+
+            Thread remoteProcessingThread = new Thread(() -> {
+                try{
+                    collections.set(service.processWebTable(Arrays.stream(filesLower)
+                            .map(File::getPath)
+                            .toArray(String[]::new), output));
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            });
+
+            localProcessingThread.start();
+            remoteProcessingThread.start();
+
+            localProcessingThread.join();
+            remoteProcessingThread.join();
+
+            for (Pair<Collection<LexicographicalOrderDependency>,String> pair : collections.get()){
+                ArrayList<String> lines = new ArrayList<>();
+                for (LexicographicalOrderDependency lod : pair.getKey()){
+                    lines.add(lod.toString());
+                }
+
+                try {
+                    String name = pair.getValue().substring(11);
+                    Files.write(Paths.get(output +"/"+ name),lines);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            System.out.println("Done calculating OD's!");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void runWithFullConvert(String ip, String input, String output,Boolean filter,Boolean dontUseNull){
+        try{
+            AODService service = (AODService) Naming.lookup("rmi://"+ ip +":1099/AODService");
+
+            File directory = new File(input);
+            File[] files = directory.listFiles();
+
+            File[] filesLower = Arrays.copyOfRange(files,0,files.length/2);
+            File[] filesUpper = Arrays.copyOfRange(files,files.length/2,files.length);
+            AtomicReference<ArrayList<Pair<Collection<LexicographicalOrderDependency>,String>>> collections = new AtomicReference<>(new ArrayList<>());
+
+            Thread localProcessingThread = new Thread(() ->{
+                RunParallel runner = new RunParallel(filesUpper, output);
+                runner.runParallel(dontUseNull);
+
 
             });
 
