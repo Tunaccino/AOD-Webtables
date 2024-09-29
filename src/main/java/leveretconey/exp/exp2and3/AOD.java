60ad9cea23bf6165aa8d44e7ad53b010635b2b7b
Tunaccino <max.glaetzner2000@gmail.com>
2024-09-29 15:23:37 +0200

    almost final changes

--- src/main/java/leveretconey/exp/exp2and3/AOD.java
+++ src/main/java/leveretconey/exp/exp2and3/AOD.java
@@ -1,24 +1,21 @@
 package leveretconey.exp.exp2and3;
 
 import leveretconey.cocoa.multipleStandard.DFSDiscovererWithMultipleStandard;
+import leveretconey.dependencyDiscover.Data.DataFormatConverter;
 import leveretconey.dependencyDiscover.Data.DataFrame;
+import leveretconey.dependencyDiscover.Dependency.LexicographicalOrderDependency;
+import leveretconey.pre.transformer.Transformer;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import static leveretconey.cocoa.multipleStandard.DFSDiscovererWithMultipleStandard.ValidatorType.G1;
 
 public class AOD {
-    public static void main(String[] args) {
-        DataFrame data = DataFrame.fromCsv("data/exp1/fd 15.csv");
-
-        // 原始的发现算法调用
-        DFSDiscovererWithMultipleStandard discoverer =new DFSDiscovererWithMultipleStandard(G1,0.01);
-        //对于DFSDiscovererWithMultipleStandard这个类，它实际使用的error rate以上面这行为准，下面这个是没用的（接口太烂）
-        discoverer.discover(data, 0.01);
-
-        List<Integer> n = new ArrayList<>();
-        System.out.println(DFSDiscovererWithMultipleStandard.count);
+    public static void main(String[] args) throws IOException {
 
     }
 }
